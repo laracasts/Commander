9539084875c2cf646bdf6a06cffbd067a88b9de4
everzet <ever.zet@gmail.com>
2014-07-28 20:50:30 +0100

    Describe ValidationCommandBus with examples
    The next natural step is to actually describe object of interest
    retrospectively. It is a tricky process on its own, but with some
    experience and luck, we came up with something like this spec.

---
+++ spec/Laracasts/Commander/ValidationCommandBusSpec.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace spec\Laracasts\Commander;
+
+use Illuminate\Foundation\Application;
+use Laracasts\Commander\CommandHandler;
+use Laracasts\Commander\CommandTranslator;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ValidationCommandBusSpec extends ObjectBehavior
+{
+    function let(Application $application, CommandTranslator $translator)
+    {
+        $this->beConstructedWith($application, $translator);
+    }
+
+    function it_does_not_handle_command_if_validation_fails(
+        Application $application,
+        CommandTranslator $translator,
+        CommandHandler $handler,
+        ExampleCommand $command,
+        ExampleValidator $validator
+    ) {
+        $translator->toValidator($command)->willReturn(ExampleValidator::class);
+        $application->make(ExampleValidator::class)->willReturn($validator);
+        $validator->validate($command)->willThrow('RuntimeException');
+
+        $translator->toCommandHandler($command)->willReturn(ExampleCommand::class);
+        $application->make(ExampleCommand::class)->willReturn($handler);
+        $handler->handle($command)->shouldNotBeCalled();
+
+        $this->shouldThrow('RuntimeException')->duringExecute($command);
+    }
+
+    function it_handles_command_if_validation_succeeds(
+        Application $application,
+        CommandTranslator $translator,
+        CommandHandler $handler,
+        ExampleCommand $command,
+        ExampleValidator $validator
+    ) {
+        $translator->toValidator($command)->willReturn(ExampleValidator::class);
+        $application->make(ExampleValidator::class)->willReturn($validator);
+
+        $translator->toCommandHandler($command)->willReturn(ExampleCommand::class);
+        $application->make(ExampleCommand::class)->willReturn($handler);
+        $handler->handle($command)->shouldBeCalled();
+
+        $this->execute($command);
+    }
+}
+
+class ExampleCommand {}
+class ExampleValidator { public function validate($command) {} }
