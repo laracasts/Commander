340cb8db803e5e1e3bd0fe5c77314f0628fb2096
everzet <ever.zet@gmail.com>
2014-07-28 21:12:44 +0100

    Make ValidationCommandBus a decorator
    We have a Single Responsibility Principle violation. Our command bus
    attempts to implement new concern - command validation. But in addition
    to doing that it also inherits original responsibility of
    DefaultCommandBus.
    
    Design patterns to the rescue! There is a wonderful design pattern that
    suites such situations wonderfully well - Decorator. The core principle
    of decorator is very simple - instead of extending class with basic
    responsibility, we instead implement the same protocol (interface) that
    base class implements:
    
        class DefaultCommandBus implements CommandBus
        class ValidationCommandBus implements CommandBus
    
    making both classes implmenet same communication protocol. Next step is
    composition. During construction of ValidationCommandBus we require
    anything that implements our CommandBus interface:
    
        class ValidationCommandBus implements CommandBus {
            function __construct(CommandBus $bus, ...
    
    to which we later delegate in custom ValidationCommandBus::execute:
    
        public function execute($command) {
            ...
            return $this->bus->execute($command);
    
    This is "composition over inheritance". Because you compose your objects
    in the runtime instead of coupling them in your codebase:
    
        $bus = new ValidationCommandBus(new DefaultCommandBus(...), ...);
    
    Why is this implementation superior to version with inheritance? Well:
    
    1. Because ValidationCommandBus does not depend on implementation of
       DefaultCommandBus, meaning you can change code of former any time
       without being afraid for horrible consequences.
    2. Because it is much easier to explain, demonstrate and spec objects
       that have single responsibility over objects that inherit
       responsibilities from all over the system.
    3. Because if you later want to introduce another responsibility to your
       bus, you still can do it:
    
           $bus = new LoggingCommandBus(
               new ValidationCommandBus(
                   new DefaultCommandBus(...),
                   ...
               ),
               ...
           )

--- spec/Laracasts/Commander/ValidationCommandBusSpec.php
+++ spec/Laracasts/Commander/ValidationCommandBusSpec.php
@@ -3,34 +3,32 @@
 namespace spec\Laracasts\Commander;
 
 use Illuminate\Foundation\Application;
-use Laracasts\Commander\CommandHandler;
+use Laracasts\Commander\CommandBus;
 use Laracasts\Commander\CommandTranslator;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class ValidationCommandBusSpec extends ObjectBehavior
 {
-    function let(Application $application, CommandTranslator $translator)
+    function let(CommandBus $bus, Application $application, CommandTranslator $translator)
     {
-        $this->beConstructedWith($application, $translator);
+        $this->beConstructedWith($bus, $application, $translator);
     }
 
     function it_does_not_handle_command_if_validation_fails(
         Application $application,
         CommandTranslator $translator,
-        CommandHandler $handler,
+        CommandBus $bus,
         ExampleCommand $command,
         ExampleValidator $validator
     ) {
-        // Responsibility group #1
+        // Own responsibility
         $translator->toValidator($command)->willReturn(ExampleValidator::class);
         $application->make(ExampleValidator::class)->willReturn($validator);
         $validator->validate($command)->willThrow('RuntimeException');
 
-        // Responsibility group #2
-        $translator->toCommandHandler($command)->willReturn(ExampleCommand::class);
-        $application->make(ExampleCommand::class)->willReturn($handler);
-        $handler->handle($command)->shouldNotBeCalled();
+        // Delegated responsibility
+        $bus->execute($command)->shouldNotBeCalled();
 
         $this->shouldThrow('RuntimeException')->duringExecute($command);
     }
@@ -38,18 +36,16 @@ class ValidationCommandBusSpec extends ObjectBehavior
     function it_handles_command_if_validation_succeeds(
         Application $application,
         CommandTranslator $translator,
-        CommandHandler $handler,
+        CommandBus $bus,
         ExampleCommand $command,
         ExampleValidator $validator
     ) {
-        // Responsibility group #1
+        // Own responsibility
         $translator->toValidator($command)->willReturn(ExampleValidator::class);
         $application->make(ExampleValidator::class)->willReturn($validator);
 
-        // Responsibility group #2
-        $translator->toCommandHandler($command)->willReturn(ExampleCommand::class);
-        $application->make(ExampleCommand::class)->willReturn($handler);
-        $handler->handle($command)->shouldBeCalled();
+        // Delegated responsibility
+        $bus->execute($command)->shouldBeCalled();
 
         $this->execute($command);
     }
